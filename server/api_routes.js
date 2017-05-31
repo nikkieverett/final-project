65377403bcd7c9f460f4471e7656b755ec074afc
Nikki Everett <nikki.everett@outlook.com>
2017-05-31 12:35:53 -0500

    idk

--- server/api_routes.js
+++ server/api_routes.js
@@ -44,16 +44,32 @@ router.get('/api/recipes/:recipeId', (req, res) => {
 });
 
 //UPDATE request to update recipes
-router.post('/api/recipes/:recipeId', (req, res) => {
+router.put('/api/recipes/:recipeId', (req, res) => {
   var cb = (err, data) => {
-    console.log(data)
+    res.sendStatus(204);
   }
-  Recipe.findByIdAndUpdate(req.params.recipeId, cb);
+  Recipe.findByIdAndUpdate(req.params.recipeId, {
+    $set: {
+      title: req.body.title,
+      prepTime: req.body.prepTime,
+      cookTime: req.body.cookTime,
+      totalTime: req.body.totalTime,
+      href: req.body.href,
+      category: req.body.category,
+      img: req.body.img,
+      ease: req.body.ease,
+      rating: req.body.rating,
+      ingredients: req.body.ingredients,
+      directions: req.body.directions,
+      notes: req.body.notes,
+      servings: req.body.servings,
+      tried: req.body.tried,
+    }
+  }, cb);
 });
 
 //DELETE request to delete recipes
 router.delete('/api/recipes/:recipeId', (req, res) => {
-  console.log('im deleting the recipe in the db')
   var cb = (err, data) => {
     res.sendStatus(204);
   };
