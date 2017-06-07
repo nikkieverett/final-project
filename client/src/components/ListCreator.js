5405f34c8e2c1c7d793727e66617038b887e365b
Nikki Everett <nikki.everett@outlook.com>
2017-06-07 11:59:08 -0500

    recipe list styling

--- client/src/components/ListCreator.js
+++ client/src/components/ListCreator.js
@@ -7,12 +7,16 @@ class ListCreator extends React.Component {
   }
   render(){
     let recipes = this.props.recipes.map((recipe) => {
+      if(recipe.rating === ''){
+        recipe.rating = 0;
+      }
       return(
         <div className="list-items" key={recipe._id}>
           <h1 onClick={() => this.handleClick(recipe._id)}>{recipe.title}</h1>
-          <h2>Category: {recipe.category}</h2>
-          <h2>Difficulty: {recipe.ease}</h2>
-          <h2>Total Time: {recipe.totalTime}</h2>
+          <h2>Category: <span>{recipe.category}</span></h2>
+          <h2>Difficulty: <span>{recipe.ease}</span></h2>
+          <h2>Total Time: <span>{recipe.totalTime}</span></h2>
+          <h2>Rating: <span>{recipe.rating} stars</span></h2>
         </div>
       )
     });
